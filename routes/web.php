71307d7bab92357b69f5972aa8a055f69dbc60ff
sylar <sylar_zeng@patazon.net>
2019-09-05 17:03:34 +0800

    添加auth用户认证

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('greeting', function () {
+    return 'Greeting';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
